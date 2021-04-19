0c834ee98c8c909d5fd585d920e0e3fe5432dba1
Glitch (hello-express) <none>
2021-04-19 16:24:34 +0000

    👣🚞 Checkpoint ./server.js:2036837/8171 ./package.json:2036837/1160

--- server.js
+++ server.js
@@ -1,34 +1,178 @@
-// server.js
-// where your node app starts
+const client1 = new(require("discord.js").Client)
+const {
+    MessageEmbed
+} = require('discord.js');
+const fetch = require('node-fetch')
+const {
+    Slash
+} = require("discord-slash-commands");
+const slash = new Slash({
+    client: client1
+})
+const embed = new MessageEmbed();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+slash.on("create", (d) => {
+    console.log(`Slash Komutu Oluşturdu: ${JSON.parse(d.config.data).name}`)
+})
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+slash.on("command", async (command) => {
+    if (command.name === "aktivite") {
+        let channel = client1.channels.cache.get(command.options.find(m => m.name === "channel").value);
+        if (channel.type !== "voice") return command.callback("Kanal bir ses kanalı olmalıdır.")
+        if (command.options.find(m => m.name === "type").value === "yt") {
+            fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        max_age: 86400,
+                        max_uses: 0,
+                        target_application_id: "755600276941176913",
+                        target_type: 2,
+                        temporary: false,
+                        validate: null
+                    }),
+                    headers: {
+                        "Authorization": `Bot ${client1.token}`,
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(res => res.json())
+                .then(invite => {
+                    embed.setTitle("Aktivite eklendi!")
+                    embed.setDescription(`[${channel.name}](https://discord.gg/${invite.code}) Kanalına Tıkla ve Youtube Ulaş!`)
+                    embed.setFooter(`Bu Komutu Kullanan Kullanıcı ${command.author.username + "#" + command.author.discriminator}`)
+                    embed.setColor("#7289DA")
+                    command.callback({
+                        embeds: embed
+                    });
+                })
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+        }
+        if (command.options.find(m => m.name === "type").value === "pn") {
+            fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        max_age: 86400,
+                        max_uses: 0,
+                        target_application_id: "755827207812677713",
+                        target_type: 2,
+                        temporary: false,
+                        validate: null
+                    }),
+                    headers: {
+                        "Authorization": `Bot ${client1.token}`,
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(res => res.json())
+                .then(invite => {
+                    embed.setTitle("Aktivite eklendi!")
+                    embed.setDescription(`[${channel.name}](https://discord.gg/${invite.code}) Kanala Tıkla ve Poker Night Oyununu Oyna!`)
+                    embed.setFooter(`Bu Komutu Kullanan Kullanıcı ${command.author.username + "#" + command.author.discriminator}`)
+                    embed.setColor("#7289DA")
+                    command.callback({
+                        embeds: embed
+                    });
+                })
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+        }
+        if (command.options.find(m => m.name === "type").value === "bio") {
+            fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        max_age: 86400,
+                        max_uses: 0,
+                        target_application_id: "773336526917861400",
+                        target_type: 2,
+                        temporary: false,
+                        validate: null
+                    }),
+                    headers: {
+                        "Authorization": `Bot ${client1.token}`,
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(res => res.json())
+                .then(invite => {
+                    embed.setTitle("Aktivite eklendi!")
+                    embed.setDescription(`[${channel.name}](https://discord.gg/${invite.code}) Kanala Tıkla ve Betrayal.io Oyununu Oyna!`)
+                    embed.setFooter(`Bu Komutu Kullanan Kullanıcı ${command.author.username + "#" + command.author.discriminator}`)
+                    embed.setColor("#7289DA")
+                    command.callback({
+                        embeds: embed
+                    });
+                })
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+        }
+        if (command.options.find(m => m.name === "type").value === "fio") {
+            fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        max_age: 86400,
+                        max_uses: 0,
+                        target_application_id: "814288819477020702",
+                        target_type: 2,
+                        temporary: false,
+                        validate: null
+                    }),
+                    headers: {
+                        "Authorization": `Bot ${client1.token}`,
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(res => res.json())
+                .then(invite => {
+                    embed.setTitle("Aktivite eklendi!")
+                    embed.setDescription(`[${channel.name}](https://discord.gg/${invite.code}) Kanala tıkla ve Fishington.io Oyununu Oyna!`)
+                    embed.setFooter(`Bu Komutu Kullanan Kullanıcı ${command.author.username + "#" + command.author.discriminator}`)
+                    embed.setColor("#7289DA")
+                    command.callback({
+                        embeds: embed
+                    });
+                })
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+        }
+    }
+})
+
+client1.on("ready", () => {
+    console.log("Bot Aktif");
+    slash.create({
+        guildOnly: false,
+        data: {
+            name: "aktivite",
+            description: "Ses kanalı etkinlikleri",
+            options: [{
+                    name: "channel",
+                    description: "İstediğiniz ses kanalını seçin.",
+                    required: true,
+                    type: 7,
+                },
+                {
+                    name: "type",
+                    description: "Aktivite çeşitleri.",
+                    required: true,
+                    type: 3,
+                    choices: [{
+                            name: "YouTube İzle",
+                            value: "yt"
+                        },
+                        {
+                            name: "Betrayal.io",
+                            value: "bio"
+                        },
+                        {
+                            name: "Poker Night",
+                            value: "pn"
+                        },
+                        {
+                            name: "Fishington.io",
+                            value: "fio"
+                        }
+                    ]
+                }
+            ]
+        }
+    })
+})
+
+client1.login('TOKEN GİR')
